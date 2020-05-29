de7922d60e848f0d90cc1c73068570fb234a7b5e
Sudharsan Balasubramani <sudharsan.bala@yahoo.com>
2020-05-29 02:21:25 -0400

    first animation! You are beautiful!

--- app/src/main/java/on/night/ui/login/LoginActivity.java
+++ app/src/main/java/on/night/ui/login/LoginActivity.java
@@ -19,6 +19,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -27,6 +28,7 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -54,6 +56,8 @@ import on.night.ui.login.LoginViewModelFactory;
 
 import java.net.URI;
 import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import on.night.R;
 import on.night.ui.map.FratMapActivity;
@@ -68,11 +72,14 @@ public class LoginActivity extends AppCompatActivity {
     private AnimationDrawable buttonAnimation;
     private FirebaseAuth mAuth;
     private GoogleSignInClient mGoogleSignInClient;
+    private AnimationDrawable mAnimationDrawable;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
 
         // Configure sign-in to request user's ID, email address, and basic profile.
         // ID and basic profile are included in Defualt sign in
@@ -91,6 +98,8 @@ public class LoginActivity extends AppCompatActivity {
 //        loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
 //                .get(LoginViewModel.class);
 
+
+
         final Button loginButton = findViewById(R.id.login);
 //        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
 
@@ -100,87 +109,12 @@ public class LoginActivity extends AppCompatActivity {
         buttonAnimation.setEnterFadeDuration(1700);
         buttonAnimation.setExitFadeDuration(1700);
 
-//        VideoView videoView = (VideoView) findViewById(R.id.videoView);
-//        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.onnight);
-//        videoView.setVideoURI(uri);
-//        videoView.start();
+        // Set up cup animation!
+        ImageView cupImage = findViewById(R.id.imageView3);
+        cupImage.setBackgroundResource(R.drawable.pong_login);
+        mAnimationDrawable = (AnimationDrawable) cupImage.getBackground();
 
 
-//        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
-//            @Override
-//            public void onChanged(@Nullable LoginFormState loginFormState) {
-//                if (loginFormState == null) {
-//                    return;
-//                }
-//                loginButton.setEnabled(loginFormState.isDataValid());
-//                if (loginFormState.getUsernameError() != null) {
-//                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-//                }
-//                if (loginFormState.getPasswordError() != null) {
-//                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-//                }
-//            }
-//        });
-//
-//        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-//            @Override
-//            public void onChanged(@Nullable LoginResult loginResult) {
-//                if (loginResult == null) {
-//                    return;
-//                }
-//                loadingProgressBar.setVisibility(View.GONE);
-//                if (loginResult.getError() != null) {
-//                    showLoginFailed(loginResult.getError());
-//                }
-//                if (loginResult.getSuccess() != null) {
-//                    updateUiWithUser(loginResult.getSuccess());
-//                }
-//                setResult(Activity.RESULT_OK);
-//
-//                //Complete and destroy login activity once successful
-//                finish();
-//            }
-//        });
-//
-//        TextWatcher afterTextChangedListener = new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//                // ignore
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                // ignore
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
-//                        passwordEditText.getText().toString());
-//            }
-//        };
-//        usernameEditText.addTextChangedListener(afterTextChangedListener);
-//        passwordEditText.addTextChangedListener(afterTextChangedListener);
-//        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-//
-//            @Override
-//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-//                if (actionId == EditorInfo.IME_ACTION_DONE) {
-//                    loginViewModel.login(usernameEditText.getText().toString(),
-//                            passwordEditText.getText().toString());
-//                }
-//                return false;
-//            }
-//        });
-//
-//        loginButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                loadingProgressBar.setVisibility(View.VISIBLE);
-//                loginViewModel.login(usernameEditText.getText().toString(),
-//                        passwordEditText.getText().toString());
-//            }
-//        });
     }
 
 //    @Override
@@ -250,13 +184,29 @@ public class LoginActivity extends AppCompatActivity {
             String welcome = getString(R.string.welcome) + model.getDisplayName();
             // TODO : initiate successful logged in experience
             Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
-            Intent mapIntent = new Intent(LoginActivity.this, FratMapActivity.class);
-            startActivityForResult(mapIntent, REQUEST_LOGIN);
+
+            // Play cup animation!
+            mAnimationDrawable.start();
+
+            checkIfAnimationDone(mAnimationDrawable);
+
         }
     }
 
-    private void showLoginFailed(@StringRes Integer errorString) {
-        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+    private void checkIfAnimationDone(AnimationDrawable anim){
+        final AnimationDrawable a = anim;
+        int timeBetweenChecks = 300;
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            public void run() {
+                if (a.getCurrent() != a.getFrame(a.getNumberOfFrames() - 1)) {
+                    checkIfAnimationDone(a);
+                } else {
+                    Intent mapIntent = new Intent(LoginActivity.this, FratMapActivity.class);
+                    startActivityForResult(mapIntent, REQUEST_LOGIN);
+                }
+            }
+        }, timeBetweenChecks);
     }
 
     @Override
